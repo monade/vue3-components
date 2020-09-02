acdf543f7f8ca122779472644655909bd331be7f
Claudio Brogliato <cbrogliato@gmail.com>
2020-09-02 17:48:50 +0200

    MultiLevel imports... not finished

---
+++ tests/unit/vmultilevelselect.spec.ts
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils'
+import { expect } from 'chai'
+import VMultiLevelSelect from '@/components/VMultiLevelSelect.vue'
+
+describe('VMultiLevelSelect', () => {
+  
+});
