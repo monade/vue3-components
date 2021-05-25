f5cb46ccf45635390a9e171996a994aa2c557305
Simone Bonvicini <simbon93@gmail.com>
2021-05-25 17:13:14 +0200

    added baseurl

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,11 @@
 // const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  publicPath: 'https://monade.github.io/vue-components',
+
   configureWebpack: {
     externals: {
+
       // jquery: 'jQuery',
       // select2: 'select2',
       // moment: 'moment'
