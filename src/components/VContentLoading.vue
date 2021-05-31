09529984308d3a34f9fa0725a875a115b8c40bca
Simone Bonvicini <simbon93@gmail.com>
2021-05-31 11:58:31 +0200

    vContentLoading docs completed

--- src/components/VContentLoading.vue
+++ src/components/VContentLoading.vue
@@ -1,23 +1,42 @@
 <template>
-  <div class="position-absolute h-100 d-flex w-100 overlay" :style="{'z-index': 10 }" v-if="true">
-    <fa-icon icon="circle-notch" :size="notchSize" class="m-auto text-primary" spin></fa-icon>
+  <div
+    class="position-absolute h-100 d-flex w-100 overlay"
+    :style="{ 'z-index': 10 }"
+  >
+    <fa-icon
+      icon="circle-notch"
+      :size="notchSize"
+      class="m-auto"
+      :class="notchClass"
+      :style="notchStyle"
+      spin
+    ></fa-icon>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component({})
 export default class VContentLoading extends Vue {
-  @Prop({ default: '3x' })
-  notchSize!: string;
+  @Prop({ default: "3x" })
+  readonly notchSize!: string;
+
+  @Prop({ default: "#00ff00" })
+  readonly notchColor!: string;
+
+  @Prop({ default: "" })
+  readonly notchClass!: string;
+
+  get notchStyle() {
+    return { color: this.notchColor };
+  }
 }
 </script>
 
 <style lang="scss">
-@import '../css/vue';
+@import "../css/vue";
 .overlay {
   background: rgba($mineshaft-2, 0.7);
 }
-
 </style>
