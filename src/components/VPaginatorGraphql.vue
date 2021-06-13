41db4d542e8827695da505b22d1e20ad78643d86
Stefano Lecchi <stefano@monade.io>
2021-06-13 16:24:38 +0200

    :fix: Adjusted meta translation. Added event @change.

--- src/components/VPaginatorGraphql.vue
+++ src/components/VPaginatorGraphql.vue
@@ -1,9 +1,11 @@
 <template>
   <v-paginator
+    v-if="isReady"
     :meta="graphQLMeta"
     :range="range"
     :loading="loading"
     :align="align"
+    @change="onPageChange($event)"
   />
 </template>
 
@@ -32,10 +34,6 @@ export default class VPaginatorGraphql extends Vue {
   @Prop({ default: null }) readonly align!: 'center' | 'right' | 'left' | null;
 
   get graphQLMeta(): PaginationMeta | null {
-    if (!this.meta) {
-      return null;
-    }
-
     const adaptedMeta: PaginationMeta = {lastPage: 0, totalPages: 0, currentPage: 0, nextPage: null, previousPage: null};
     adaptedMeta.currentPage = this.meta.currentPage!;
     adaptedMeta.totalPages = this.meta.totalPages!;
@@ -43,11 +41,19 @@ export default class VPaginatorGraphql extends Vue {
 
     adaptedMeta.nextPage = this.meta.currentPage === this.meta.totalPages ?
       this.meta.totalPages :
-      this.meta.currentPage! - 1;
+      this.meta.currentPage! + 1;
 
-    adaptedMeta.lastPage = this.meta.currentPage!;
+    adaptedMeta.lastPage = this.meta.totalPages!;
 
     return adaptedMeta;
+  }
+
+  get isReady() {
+    return this.meta ? true : false;
+  }
+
+  private onPageChange(page: number) {
+    this.$emit('change', page);
   }
 }
 
