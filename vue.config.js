43c3d4edb82edebfcc35f37617ca27c654a96752
ProGM <progiemmeh@gmail.com>
2022-01-30 18:48:01 +0100

    :new: configuration for docs

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 // const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  // publicPath: 'https://monade.github.io/vue-components',
+  publicPath: process.env.PUBLIC_PATH ?? '/',
   configureWebpack: {
     externals: {
 
