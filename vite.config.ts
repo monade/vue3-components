a9711804bf8ac42194b67bcd2f2afe6e3094650b
Piero Dotti <progiemmeh@gmail.com>
2024-03-11 12:01:26 +0100

    fix: pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const envs = [
-    'GLOBAL_ENV',
-    'BASE_API_URL',
-    'FRONTEND_URL',
-    'SENTRY_DSN',
-    'TASK_MANAGER_API_URL',
-    'CONTACT_MANAGER_API_URL',
-    'BOAT_MANAGER_API_URL',
-  ].filter(v => !!env[v]);
-
   return {
     plugins: [
       vue(),
@@ -31,11 +20,7 @@ export default defineConfig(({ command, mode }) => {
       port: 8082,
       host: '127.0.0.1'
     },
-    define: {
-      'my_envs': envs.reduce((acc, v) => {
-        return { ...acc, [v]: env[v] }
-      }, {}),
-    },
+    base: process.env.BASE_PATH ?? '/',
     build: {
       sourcemap: true,
     }
